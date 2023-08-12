a342c0749bb0c4f9a6b19724302b15b70843cbef
Khalasismit <smit67560@gmail.com>
2023-08-12 16:46:16 +0530

    changed mongodb url

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   const Name = "TweetVerse";
   // const _id = "64d637d66922009c9a577597"; // Curious.khalasi
-  const _id = "64d4e50e5f4d7e6602c21355"; // Smit.khalasi
+  // const _id = "64d4e50e5f4d7e6602c21355"; // Smit.khalasi
+  const _id = "64d768dc1ba3525e2764dc04" // curious.khalasi
   return (
     <div className="App">
       <BrowserRouter>
